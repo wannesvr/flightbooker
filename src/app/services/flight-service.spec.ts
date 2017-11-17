1faa740795661dfae5ba5b16dc7aa67aa26cb71d
Wannes Van Regenmortel <wannesvr@hotmail.com>
2017-11-17 16:37:28 +0100

    FlightService fully tested

--- src/app/services/flight-service.spec.ts
+++ src/app/services/flight-service.spec.ts
@@ -33,9 +33,40 @@ describe('FlightService', () => {
         service.getFlights().subscribe((httpResponse) => {
             response = httpResponse;
         });
-        
-        
+
+
         http.expectOne('http://localhost:3000/flights').flush(flights);
         expect(response).toEqual(flights);
+    });
+
+    it('searchFlights should return an array of flights', () => {
+        const flights: Flight[] = [
+            new Flight('FlightOne', 'FlightFrom', 'FlightTo', 0, AirplaneType.Airbus_A320),
+            new Flight('FlightTwo', 'FlightFrom', 'FlightTo', 0, AirplaneType.Airbus_A320)
+        ];
+
+        const expectedResponse: Flight[] = [flights[1]];
+
+        let response;
+
+        service.searchFlights('FlightTwo').subscribe((httpResponse) => {
+            response = httpResponse;
+        });
+
+        http.expectOne('http://localhost:3000/flights').flush(flights);
+        expect(response).toEqual(expectedResponse);
+    });
+
+    it('updateFlight should return the updated flight', () => {
+        const flight: Flight = new Flight('FlightOne', 'FlightFrom', 'FlightTo', 0, AirplaneType.Airbus_A320);
+
+        let response;
+
+        service.updateFlight(flight).subscribe((httpResponse) => {
+            response = httpResponse;
+        });
+
+        http.expectOne('http://localhost:3000/flights/' + flight.id).flush(flight);
+        expect(response).toEqual(flight);
     });
 });
